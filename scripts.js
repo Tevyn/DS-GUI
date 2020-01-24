22ce51682f9606397bd7d27b574d02fefb8e134e
Tevyn <tevynbell@gmail.com>
2020-01-24 08:43:56 -0700

    passing functons instead of eval
    destroyed the eval menace by just passing arbitrary arrow functions around

--- scripts.js
+++ scripts.js
@@ -1,14 +1,14 @@
 //Linking an input's elementId to a css variable
 function linkInput(elementId, cssVar){
-  linkInputHelper(elementId, cssVar, 'e.target.value')
+  linkInputHelper(elementId, cssVar, (e) => e.target.value)
 }
 
-//A helper that accepts an arbitrary value as a string to allow for calculations and units
+//A helper that accepts an arbitrary function to allow for calculations and units
 function linkInputHelper(elementId, cssVar, cssFunction) {
   const input = document.getElementById(elementId);
 
   input.addEventListener('input', e => {
-    document.documentElement.style.setProperty(cssVar, eval(cssFunction));
+    document.documentElement.style.setProperty(cssVar, cssFunction(e));
 })}
 
 //Brand basics
@@ -17,18 +17,18 @@ linkInput('bkg', '--background');
 
 //Body text
 linkInput('body-font','--body-font');
-linkInput('rem','--rem');
+linkInputHelper('rem','--rem', (e) => e.target.value + 'px');
 linkInput('body-color','--body-color');
 linkInput('body-weight','--body-weight');
 
 //Header text
 linkInput('header-font','--header-font');
-linkInputHelper('scale','--h1',`Math.pow(e.target.value, 5) + 'rem'`);
-linkInputHelper('scale','--h2',`Math.pow(e.target.value, 4) + 'rem'`);
-linkInputHelper('scale','--h3',`Math.pow(e.target.value, 3) + 'rem'`);
-linkInputHelper('scale','--h4',`Math.pow(e.target.value, 2) + 'rem'`);
-linkInputHelper('scale','--h5',`e.target.value + 'rem'`);
+linkInputHelper('scale','--h1',(e) => Math.pow(e.target.value, 5) + 'rem');
+linkInputHelper('scale','--h2',(e) => Math.pow(e.target.value, 4) + 'rem');
+linkInputHelper('scale','--h3',(e) => Math.pow(e.target.value, 3) + 'rem');
+linkInputHelper('scale','--h4',(e) => Math.pow(e.target.value, 2) + 'rem');
+linkInputHelper('scale','--h5',(e) => e.target.value + 'rem');
 
 //Buttons and inputs
 linkInput('border-color','--border-color');
-linkInputHelper('radius','--radius',`e.target.value + 'rem'`);
+linkInputHelper('radius','--radius',(e) => e.target.value + 'rem');
